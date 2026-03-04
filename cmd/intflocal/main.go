7adbc68368ea36fc6742fd4b7e126d88dc1723e1
Sergey Slonimsky <sergeyslonimsky@gmail.com>
2026-03-04 21:23:31 +0100

    add version

--- cmd/intflocal/main.go
+++ cmd/intflocal/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"golang.org/x/tools/go/analysis/singlechecker"
 
@@ -13,9 +14,22 @@ var version = "dev"
 
 func main() {
 	if len(os.Args) == 2 && (os.Args[1] == "-version" || os.Args[1] == "--version" || os.Args[1] == "-v") {
-		fmt.Println("intflocal version " + version)
+		fmt.Println("intflocal version " + getVersion())
 		os.Exit(0)
 	}
 
 	singlechecker.Main(analyzer.New(analyzer.Settings{}))
+}
+
+func getVersion() string {
+	if version != "dev" {
+		return version
+	}
+
+	info, ok := debug.ReadBuildInfo()
+	if ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+
+	return version
 }

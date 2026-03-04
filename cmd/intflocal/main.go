1474fcb66bc6534ad324fde8f9a1d38f95122e12
Sergey Slonimsky <sergeyslonimsky@gmail.com>
2026-03-04 21:16:15 +0100

    add version

--- cmd/intflocal/main.go
+++ cmd/intflocal/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"golang.org/x/tools/go/analysis/singlechecker"
 
 	"github.com/sergeyslonimsky/intflocal/analyzer"
 )
 
+var version = "dev"
+
 func main() {
+	if len(os.Args) == 2 && (os.Args[1] == "-version" || os.Args[1] == "--version" || os.Args[1] == "-v") {
+		fmt.Println("intflocal version " + version)
+		os.Exit(0)
+	}
+
 	singlechecker.Main(analyzer.New(analyzer.Settings{}))
 }
